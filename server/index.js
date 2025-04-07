2653f3b0f89314b522c6f9a14c36ad126a4e71ef
Srikar <srikarjanjirala92@gmail.com>
2025-04-07 17:36:27 +0530

    CORS issue solved

--- server/index.js
+++ server/index.js
@@ -23,10 +23,11 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://digital-library-delta.vercel.app/"],
+    origin: ["http://localhost:5173", "https://digital-library-delta.vercel.app"], // Removed trailing slash
     methods: ["GET", "POST"]
   }
 });
+
 
 // Middleware
 app.use(cors());
